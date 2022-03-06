be0aefc03a8eaf335855360c2ea5b4bcecb991bd
Lakshsharma31 <100475528+Lakshsharma31@users.noreply.github.com>
2022-03-06 12:47:44 +0530

    Update MessageHandler.ts

--- src/Handlers/MessageHandler.ts
+++ src/Handlers/MessageHandler.ts
@@ -284,7 +284,7 @@ export default class MessageHandler {
       ];
       const buttonMessage: any = {
         contentText: `*A claimable character Appeared!*\n\n🎀 *Name: ${chara.name}*\n\n💬 *About:* ${chara.about}\n\n📛 *Source: ${source[0].anime.title}*\n\n💰 *Price: ${price}*\n\n*[Use ${this.client.config.prefix}claim to have this character in your gallery]*`,
-        footerText: "🎇 Beyond 🎇",
+        footerText: "ᴹᴿ᭄ʟᴇᴠɪ ོ ×፝֟͜×",
         buttons: buttons,
         headerType: 4,
         imageMessage: media?.message?.imageMessage,
@@ -304,7 +304,7 @@ export default class MessageHandler {
   };
 
   handleState = async (): Promise<void> => {
-    const text = `🎇BACK TO BUSINESS!!🎇 (t-ehe)`;
+    const text = `🚀🚀𝕝𝕖𝕧𝕚 𝕚𝕤 𝕟𝕠𝕨 𝕒𝕔𝕥𝕚𝕧𝕖`;
     await this.client.sendMessage(
       "120363038329584084@g.us",
       text,
