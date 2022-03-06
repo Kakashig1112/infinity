643864bd05780e130bc07b330ead7a375203ac6f
Lakshsharma31 <100475528+Lakshsharma31@users.noreply.github.com>
2022-03-06 12:39:51 +0530

    Update Slot.ts

--- src/commands/Economy/Slot.ts
+++ src/commands/Economy/Slot.ts
@@ -121,7 +121,7 @@ export default class Command extends BaseCommand {
       const text = `${head}\n\n${o}\n\n📈 You won *${gold} gold*.`;
       const buttonMessage: any = {
         contentText: `${text}`,
-        footerText: "🎇 Beyond 🎇",
+        footerText: "𝕝𝕖𝕧𝕚",
         buttons: buttons,
         headerType: 1,
       };
@@ -137,7 +137,7 @@ export default class Command extends BaseCommand {
       const text = `${head}\n\n${p}\n\n🎊 *Jackpot!* You won *${gold} gold*.`;
       const buttonMessage: any = {
         contentText: `${text}`,
-        footerText: "🎇 Beyond 🎇",
+        footerText: "𝕝𝕖𝕧𝕚",
         buttons: buttons,
         headerType: 1,
       };
